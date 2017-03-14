2d25ef04197dcf2477c1a528d94777a180ae93a9
babadela3 <teosposib@yahoo.com>
2017-03-14 20:07:38 +0200

    added loginscreen

---
+++ src/sample/LoginScreen.java
@@ -0,0 +1,261 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class LoginScreen extends Application {
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+        //Basic Pane
+
+        primaryStage.setTitle("JavaFX 2 Login");
+        BorderPane bp = new BorderPane();
+        bp.setPadding(new Insets(120,300,160,300));
+
+        //Adding HBox
+
+        HBox hb = new HBox();
+        hb.setPadding(new Insets(20,20,20,30));
+
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setOffsetX(5);
+        dropShadow.setOffsetY(7);
+
+
+        Text text = new Text("Play Multiplayer");
+        text.setFont(Font.font("Courier New", FontWeight.BOLD, 28));
+        text.setEffect(dropShadow);
+
+        hb.getChildren().add(text);
+        bp.setTop(hb);
+
+
+        //Adding GridPane
+
+
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPrefSize(1000,500);
+        gridPane.setHgap(10);
+        gridPane.setVgap(5);
+
+
+        //Implementing Nodes for GridPane
+
+
+
+        Label lblTitle = new Label("   Blackjack");
+        lblTitle.setFont(Font.font("Script MT Bold", FontWeight.BOLD, 50));
+        lblTitle.setTextFill(Color.web("663F15"));
+
+        final TextField txtUserName = new TextField();
+        txtUserName.setPromptText("E-mail");
+        txtUserName.setFocusTraversable(false);
+        txtUserName.setFont(Font.font("Courier New", FontWeight.NORMAL, 15));
+
+        final PasswordField pf = new PasswordField();
+        pf.setPromptText("Password");
+        pf.setFocusTraversable(false);
+        pf.setFont(Font.font("Courier New", FontWeight.NORMAL, 15));
+
+        GridPane gridPaneForgotPassword = new GridPane();
+        gridPaneForgotPassword.setPrefSize(1000,500);
+        gridPaneForgotPassword.setHgap(10);
+        gridPaneForgotPassword.setVgap(5);
+
+        final Label forgotPassword = new Label("Forgot password?");
+        forgotPassword.setFont(Font.font("Courier New", FontWeight.NORMAL, 11));
+        forgotPassword.setTextFill(Color.web("663F15"));
+        forgotPassword.setUnderline(true);
+        gridPaneForgotPassword.add(forgotPassword, 18, 0);
+
+
+        Button btnLogin = new Button("Login");
+        btnLogin.setTextFill(Color.web("ffffff"));
+        btnLogin.setFont(Font.font("Courier New", FontWeight.BOLD, 16));
+
+        Label optionText = new Label("                  or");
+        optionText.setFont(Font.font("Courier New", FontWeight.NORMAL, 13));
+        optionText.setTextFill(Color.web("663F15"));
+
+        Button fbLogin = new Button("Login with Facebook");
+        fbLogin.setTextFill(Color.web("ffffff"));
+        fbLogin.setFont(Font.font("Courier New", FontWeight.BOLD, 16));
+
+        GridPane gridPaneSignUp = new GridPane();
+        gridPaneSignUp.setPrefSize(1000,500);
+        gridPaneSignUp.setHgap(5);
+        gridPaneSignUp.setVgap(5);
+
+        final Label messageSignUp = new Label("Don't have an account yet?");
+        messageSignUp.setFont(Font.font("Courier New", FontWeight.NORMAL, 13));
+        messageSignUp.setTextFill(Color.web("663F15"));
+
+        final Label linkSignUp = new Label("Sign Up");
+        linkSignUp.setFont(Font.font("Courier New", FontWeight.BOLD, 13));
+        linkSignUp.setTextFill(Color.web("663F15"));
+
+
+        gridPaneSignUp.add(messageSignUp, 3, 0);
+        gridPaneSignUp.add(linkSignUp, 4, 0);
+
+
+        //Adding Nodes to GridPane layout
+        gridPane.add(lblTitle, 64, 4);
+        gridPane.add(txtUserName, 64, 17);
+        gridPane.add(pf, 64, 19);
+        gridPane.add(gridPaneForgotPassword,64,20);
+        gridPane.add(btnLogin, 64, 32);
+        gridPane.add(optionText, 64, 34);
+        gridPane.add(fbLogin, 64, 36);
+        gridPane.add(gridPaneSignUp,64,50);
+
+
+
+
+
+        //Add ID's to Nodes
+        bp.setId("bp");
+        txtUserName.setId("dim");
+        pf.setId("dim");
+        gridPane.setId("root");
+        btnLogin.setId("btnLogin");
+        text.setId("text");
+        fbLogin.setId("fbLogin");
+
+
+        //Actions for buttons and texts
+
+        btnLogin.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Login");
+            }
+        });
+
+        fbLogin.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Login with Facebook");
+            }
+
+        });
+
+        forgotPassword.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+               System.out.println("Forgot Password");
+            }
+        });
+
+        linkSignUp.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Sign Up");
+            }
+        });
+
+        fbLogin.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.HAND); //Change cursor to hand
+            }
+        });
+
+        fbLogin.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.DEFAULT); //Change cursor to hand
+            }
+        });
+
+        btnLogin.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.HAND); //Change cursor to hand
+            }
+        });
+
+        btnLogin.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.DEFAULT); //Change cursor to hand
+            }
+        });
+
+        forgotPassword.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.HAND); //Change cursor to hand
+            }
+        });
+
+        forgotPassword.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.DEFAULT); //Change cursor to hand
+            }
+        });
+
+        linkSignUp.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.HAND); //Change cursor to hand
+            }
+        });
+
+        linkSignUp.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                primaryStage.getScene().setCursor(Cursor.DEFAULT); //Change cursor to hand
+            }
+        });
+
+
+
+
+        //AGridPane layout to BorderPane Layout
+
+        bp.setCenter(gridPane);
+
+        //Adding BorderPane to the scene and loading CSS
+        Scene scene = new Scene(bp);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("resources/login.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Blackjack");
+       // primaryStage.setMaximized(true);
+        primaryStage.titleProperty().bind(
+                scene.widthProperty().asString().
+                concat(" : ").
+                concat(scene.heightProperty().asString()));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
